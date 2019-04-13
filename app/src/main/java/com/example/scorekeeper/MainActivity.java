35febc399df33b6580e0d599118c16185acfe2ce
USER <cucusadewa0294@gmail.com>
2019-04-14 03:05:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.scorekeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    // Member variables for holding the score
+    private int mScore1;
+    private int mScore2;
+
+    static final String STATE_SCORE_1 = "Team 1 Score";
+    static final String STATE_SCORE_2 = "Team 2 Score";
+
+    // Variables for holding reference of TextView
+    private TextView mScoreText1;
+    private TextView mScoreText2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Find the TextViews by ID
+        mScoreText1 = (TextView) findViewById(R.id.tv_score1);
+        mScoreText2 = (TextView) findViewById(R.id.tv_score2);
+
+        if (savedInstanceState != null) {
+            mScore1 = savedInstanceState.getInt(STATE_SCORE_1);
+            mScore2 = savedInstanceState.getInt(STATE_SCORE_2);
+
+            // set the score text views
+            mScoreText1.setText(String.valueOf(mScore1));
+            mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    public void descreaseScore(View view) {
+
+        int viewID = view.getId();
+        switch (viewID) {
+            case R.id.imgBtn_descrease_team1:
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            // if it wason Team 2
+            case R.id.imgBtn_descrease_team2:
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+
+    }
+
+    public void increaseScore(View view) {
+
+        int viewID = view.getId();
+        switch (viewID) {
+            case R.id.imgBtn_increase_team1:
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            // if it was on Team 2
+            case R.id.imgBtn_increase_team2:
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        // Change the label of the menu based on the state of the app.
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else {
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Check if the correct item was clicked
+        if (item.getItemId() == R.id.night_mode) {
+            // Get the night mode state of the app.
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            //Set the theme mode for the restarted activity
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode
+                        (AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode
+                        (AppCompatDelegate.MODE_NIGHT_YES);
+            }
+            // Recreate the activity for the theme change to take effect.
+            recreate();
+        }
+        return true;
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // save the score
+        outState.putInt(STATE_SCORE_1, mScore1);
+        outState.putInt(STATE_SCORE_2, mScore2);
+        super.onSaveInstanceState(outState);
+    }
+
+
+}
